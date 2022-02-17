f4372a975adfd2d70abfbd233b66823ec747d4b8
Ubuntu <ubuntu@ip-172-31-74-127.ec2.internal>
2022-02-17 09:09:22 +0000

    print fix

--- print.js
+++ print.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
   
     $( ".printpdf" ).click(function() {
+
+    // $(".doc-section").show();
+        // $(".doc-section").removeAttr('style');
      window.print();
+  
            return false;
     });
 });
