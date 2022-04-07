1b05e93398b06b5acc434b62f08023855b2c5626
Ubuntu <ubuntu@ip-172-31-74-127.ec2.internal>
2022-04-07 10:00:23 +0000

    indexed version

--- document-nav.js
+++ document-nav.js
@@ -1,14 +1,33 @@
 $(document).ready(function() {
-  
 
   var href;
   var section;
   var next;
   var prev;
 
+  function linkClick(href) {
+    
+     section = href.split('-')[0];
+
+    if (section.substring(0, 1) == "#") {
+
+      next = parseInt(section.substring(8)) + 1;
+      prev = next - 2;
+      pageChanger(prev, next, section);
+
+      $(".nav-item a").each(function(index) {
+        if ($(this).attr("href") == section) {
+          $(this).addClass("disabled").append('<span class="sr-only">(current)</span>');
+        }
+
+      });
+
+    }
+
+  }
 
   function pageChanger(prev, next, section) {
-    
+
     // disables previous and next when necessary
 
     if (prev > 0) {
@@ -34,77 +53,54 @@ $(document).ready(function() {
       $(".next-page").parent().remove(".sr-only");
       $(".next-page").attr("id", "tab-index='1'");
     }
-    
+
     // changes ids of prev and next buttons
 
-    next = "p" + next;
-    prev = "p" + prev;
-    $(".next-page").parent().attr("id", next);
-    $(".prev-page").parent().attr("id", prev);
-    
-    // hides and show relavant sections
+    next = "#section" + next;
+    prev = "#section" + prev;
+    $(".next-page").attr("href", next);
+    $(".prev-page").attr("href", prev);
 
-    $(".doc-section").hide();
-    $(section).show();
-    
+    // hides and show relevant sections
+
+    $(".doc-section").addClass("visuallyhidden");
+    $(section).removeClass("visuallyhidden");
+
     // Removes disabled clasees from left hand nav
     $(".nav-item a").removeClass("disabled");
     $(".nav-item a span").remove(".sr-only");
 
   }
-  
-  // When left hand nav is clicked
+
+  // When left hand navk is clicked
 
   $(".nav-item a").click(function() {
     href = $(this).attr("href");
-    section = "#section" + href.substring(1);
-    next = parseInt(href.substring(1)) + 1;
-    prev = next - 2;
-    pageChanger(prev, next, section);
-    $(this).addClass("disabled").append('<span class="sr-only">(current)</span>');
-  });
 
-
- // When Next is clicked
- 
-  $(".next-page").click(function() {
-    page = $(this).parent().attr("id");
-
-    section = "#section" + page.substring(1);
-
-    href = "#" + page.substring(1);
-
-
-    var next = parseInt(href.substring(1)) + 1;
-    var prev = next - 2;
-
-    pageChanger(prev, next, section);
-
-    $(".nav-item a").each(function(index) {
-      if ($(this).attr("href") == href) {
-        $(this).addClass("disabled").append('<span class="sr-only">(current)</span>');
-      }
-    });
+    linkClick(href);
 
   });
   
-  // When Previous is clicked
+  // When link on page is clicked
 
-  $(".prev-page").click(function() {
-    page = $(this).parent().attr("id");
-    section = "#section" + page.substring(1);
-    href = "#" + page.substring(1);
+  $(".doc-section a").click(function() {
+    href = $(this).attr("href");
 
-    prev = parseInt(href.substring(1)) - 1;
-    next = prev + 2;
+linkClick(href);
+   
+ const element = document.getElementById(href);
+      element.scrollIntoView();
 
-    pageChanger(prev, next, section);
+  });
 
-    $(".nav-item a").each(function(index) {
-      if ($(this).attr("href") == href) {
-        $(this).addClass("disabled").append('<span class="sr-only">(current)</span>');
-      }
-    });
+
+  // When Next or Previous is clicked
+
+  $(".page-link").click(function() {
+    
+      href = $(this).attr("href");
+
+    linkClick(href);
 
   });
 
